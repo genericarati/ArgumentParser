9d7c49ce188bb5f7b547503caf20a6d06f31640e
genericarati <genericarati@gmail.com>
2017-10-24 22:45:47 -0400

    Added boolean argument marshaller and string argument marshaller

--- src/Args.java
+++ src/Args.java
@@ -4,8 +4,8 @@ import java.util.Map;
 public class Args {
 	private String[] arguments;
 	private String schema;
-	private Map<Character, Boolean> booleanArgs;
-	private Map<Character, String> stringArgs;
+	private Map<Character, ArgumentMarshaler> booleanArgs;
+	private Map<Character, ArgumentMarshaler> stringArgs;
 	private Map<Character, Integer> intArgs;
 
 	public Args(String schema, String[] arguments) throws ArgsException {
@@ -17,27 +17,13 @@ public class Args {
 		parse();
 	}
 
-	protected boolean isValidSchema() {
-		if (this.schema == null || this.schema.isEmpty())
-			return false;
-		else
-			return true;
-	}
-
-	protected boolean isValidArguments() {
-		if (this.arguments == null || this.arguments.length == 0)
-			return false;
-		else
-			return true;
-	}
-
 	public void parse() throws ArgsException {
 		if (isValidSchema()) {
 			parseSchema();
 		} else {
 			throw new ArgsException("Schema is not valid");
 		}
-
+		
 		if (isValidArguments()) {
 			parseArguments();
 		} else {
@@ -45,35 +31,11 @@ public class Args {
 		}
 	}
 
-	private void parseArguments() throws ArgsException {
-		for (int i = 0; i < arguments.length; i++) {
-			char schemaChar = arguments[i].charAt(1);
-			if (booleanArgs.containsKey(schemaChar)) {
-				setBooleanArgs(schemaChar);
-			} else if (intArgs.containsKey(schemaChar)) {
-				setIntArgs(schemaChar);
-			} else if (stringArgs.containsKey(schemaChar)) {
-				setStringArgs(i);
-			} else {
-				throw new ArgsException("Arguments don't match as per schema");
-			}
-		}
-	}
-
-	private void setStringArgs(int i) {
-		stringArgs.put(arguments[i].charAt(1), arguments[i]);
-	}
-
-	private void setBooleanArgs(char schemaChar) {
-		booleanArgs.put(schemaChar, true);
-	}
-
-	private void setIntArgs(char schemaChar) throws ArgsException {
-			try{
-				intArgs.put(schemaChar, Integer.parseInt(String.valueOf(schemaChar)));
-			}catch(NumberFormatException e){
-				throw new ArgsException("Argument could not be parsed to integer.");
-			}
+	protected boolean isValidSchema() {
+		if (this.schema == null || this.schema.isEmpty())
+			return false;
+		else
+			return true;
 	}
 
 	protected void parseSchema() throws ArgsException {
@@ -81,14 +43,58 @@ public class Args {
 		for (int i = 0; i < schemaStrings.length; i++) {
 			String s = schemaStrings[i];
 			if (s.length() == 1)
-				booleanArgs.put(s.charAt(0), false);
+				setBooleanArgs(s.charAt(0), false);
 			else if (s.charAt(1) == ('#')) {
 				intArgs.put(s.charAt(0), 0);
 			} else {
-				stringArgs.put(s.charAt(0), "");
+				setStringArgs(s.charAt(0), "");
 			}
 		}
 	}
+	
+	protected boolean isValidArguments() {
+		if (this.arguments == null || this.arguments.length == 0)
+			return false;
+		else
+			return true;
+	}
+
+
+	private void parseArguments() throws ArgsException {
+		for (int i = 0; i < arguments.length; i++) {
+			char schemaChar = arguments[i].charAt(1);
+			if (booleanArgs.containsKey(schemaChar)) {
+				setBooleanArgs(schemaChar, true);
+			} else if (intArgs.containsKey(schemaChar)) {
+				setIntArgs(schemaChar);
+			} else if (stringArgs.containsKey(schemaChar)) {
+				setStringArgs(schemaChar, arguments[i]);
+			} else {
+				throw new ArgsException("Arguments don't match as per schema");
+			}
+		}
+	}
+
+	private void setStringArgs(char schemaChar, String value) {
+		ArgumentMarshaler stringArgumentMarshaler = new StringArgumentMarshaler();
+		stringArgumentMarshaler.setStringValue(value);
+		stringArgs.put(schemaChar, stringArgumentMarshaler);
+	}
+
+	private void setBooleanArgs(char schemaChar, boolean value) {
+		ArgumentMarshaler booleanArgumentMarshaler = new BooleanArgumentMarshaler();
+		booleanArgumentMarshaler.setBooleanValue(value);
+		booleanArgs.put(schemaChar, booleanArgumentMarshaler);
+	}
+
+	private void setIntArgs(char schemaChar) throws ArgsException {
+		try {
+			intArgs.put(schemaChar, Integer.parseInt(String.valueOf(schemaChar)));
+		} catch (NumberFormatException e) {
+			throw new ArgsException("Argument could not be parsed to integer.");
+		}
+	}
+
 
 	protected boolean zeroIfNull(char schemaChar) {
 		if (intArgs != null && intArgs.containsKey(schemaChar))
@@ -106,13 +112,13 @@ public class Args {
 
 	protected boolean getBoolean(char schemaChar) {
 		if (falseIfNull(schemaChar))
-			return this.booleanArgs.get(schemaChar);
+			return this.booleanArgs.get(schemaChar).getBooleanValue();
 		else
 			return false;
 	}
 
 	public String getString(char schemaChar) {
-		return this.stringArgs.get(schemaChar);
+		return this.stringArgs.get(schemaChar).getStringValue();
 	}
 
 	public Integer getInteger(char schemaChar) {
@@ -127,6 +133,38 @@ public class Args {
 			super(errorMessage);
 		}
 
+	}
+
+	protected class ArgumentMarshaler {
+		private boolean booleanValue = false;
+		private String stringValue = "";
+
+		public boolean getBooleanValue() {
+			return booleanValue;
+		}
+
+		
+		public String getStringValue() {
+			return stringValue;
+		}
+
+
+		public void setStringValue(String stringValue) {
+			this.stringValue = stringValue;
+		}
+
+		public void setBooleanValue(boolean booleanValue) {
+			this.booleanValue = booleanValue;
+		}
+	}
+
+	protected class BooleanArgumentMarshaler extends ArgumentMarshaler {
+	}
+
+	private class IntegerArgumentMarshaler extends ArgumentMarshaler {
+	}
+
+	private class StringArgumentMarshaler extends ArgumentMarshaler {
 	}
 
 }
--- test/ArgsTest.java
+++ test/ArgsTest.java
@@ -5,6 +5,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+
 public class ArgsTest {
 
 	@Rule
